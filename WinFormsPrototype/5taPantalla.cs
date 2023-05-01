2788ec7f7046b87a5c3a4fb050dc07da7d40618c
MeliDaRosa-02 <132232691+MeliDaRosa-02@users.noreply.github.com>
2023-05-01 00:32:23 -0300

    Agregue pantalla 4y5

---
+++ WinFormsPrototype/5taPantalla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsPrototype
+{
+    public partial class _5taPantalla : Form
+    {
+        public _5taPantalla()
+        {
+            InitializeComponent();
+        }
+    }
+}
